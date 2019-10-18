7436b4fd9967117c498e7cc746019f8b126836a4
Bryan McBride <mcbride.bryan@gmail.com>
2019-10-17 22:50:34 -0400

    upgrade to latest F7 & OL libs

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-03.06.19.1';
+const PRECACHE = 'precache-10.17.19.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -26,15 +26,17 @@ const PRECACHE_URLS = [
   'maps.json',
   'assets/img/apple-touch-icon.png',
   'assets/img/android-chrome-192x192.png',
+  'assets/img/favicon-32x32.png',
+  'assets/img/favicon-16x16.png',
   'assets/img/icon.png',
   'assets/img/crosshair.svg',
   'assets/img/ios-share.png',
-  'assets/vendor/framework7-4.1.0/css/framework7.bundle.min.css',
-  'assets/vendor/openlayers-5.3.0/ol.css',
+  'assets/vendor/framework7-5.0.5/css/framework7.bundle.min.css',
+  'assets/vendor/openlayers-6.0.1/ol.css',
   'assets/css/app.css',
   'assets/fonts/MaterialIcons-Regular.woff2',
-  'assets/vendor/framework7-4.1.0/js/framework7.bundle.min.js',
-  'assets/vendor/openlayers-5.3.0/ol.js',
+  'assets/vendor/framework7-5.0.5/js/framework7.bundle.min.js',
+  'assets/vendor/openlayers-6.0.1/ol.js',
   'assets/vendor/proj4js-2.5.0/proj4.js',
   'assets/js/app.js'
 ];
