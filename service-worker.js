d69803f475b6aeb205b08254441bcab6bf5ed6ee
Bryan McBride <mcbride.bryan@gmail.com>
2019-12-06 11:13:46 -0500

    bump cache version

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-10.17.19.1';
+const PRECACHE = 'precache-12.06.19.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
